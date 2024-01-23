68e949f1ca701196c40a6d01b3256dbc6f49c284
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-01-23 12:00:00 -0300

    apple news

---
+++ news-app/APICaller.swift
@@ -0,0 +1,58 @@
+//
+//  APICaller.swift
+//  news-app
+//
+//  Created by Andrés Rechimon on 23/01/2024.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/everything?q=apple&from=2024-01-22&to=2024-01-22&sortBy=popularity&apiKey=5a8ae994de384efd847c513c0f3602b8")
+    }
+    
+    private init(){}
+    
+    public func getTopNews(completion: @escaping(Result<[Article], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                do{
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    completion(.success(result.articles))
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    let name: String
+}
