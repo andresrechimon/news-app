9f1584d84a135ba14420fb0247e10c6883b2c4a8
Rechimon, Andrés <andres.r1995@hotmail.com>
2024-01-23 10:04:41 -0300

    Initial Commit

---
+++ news-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news-app
+//
+//  Created by Andrés Rechimon on 23/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
